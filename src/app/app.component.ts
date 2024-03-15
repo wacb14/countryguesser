f9ea080f8131b588880cb5cd2383084bc8bfdebe
Willy Cruz <wacb14@gmail.com>
2024-03-15 01:20:57 -0500

    Maps generated and highlighted by code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  title = 'countryguesser';
-  constructor(){}
-  ngOnInit(): void {
-    // Usando jQuery 
-    $('#vmap').vectorMap({ map: 'world_en' });
+export class AppComponent implements AfterViewInit {
+  title: string = 'countryGuesser';
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.loadMap('africa_en','cg');
+  }
+
+  //-- Changes the map showed in the card
+  loadMap(name: string, regionCode:string) {
+    this.emptyMapViewer();
+    var map: any = $('#mapViewer');
+    map.vectorMap({
+      map: name,
+      color: '#f4f3f0',
+      enableZoom: true,
+      hoverColor: '#c9dfaf',
+      hoverOpacity: true,
+      normalizeFunction: 'linear',
+      selectedColor: null,
+      // selectedColor: null,
+        // colors: {
+        //   za: '#2980b9',
+        // },
+      showTooltip: false,
+    });
+    map.vectorMap('set', 'colors', regionCode, '#ff0000');
+  }
+
+  //-- Clear the viewer
+  emptyMapViewer() {
+    $('#mapViewer').empty();
   }
 }
-
-
