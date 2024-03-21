4ca0b112543458d0b669e42a311c009ee4fb8293
Willy Cruz <wacb14@gmail.com>
2024-03-21 12:30:12 -0500

    Disabled buttons implemented

--- src/app/components/alternatives-viewer/alternatives-viewer.component.ts
+++ src/app/components/alternatives-viewer/alternatives-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Country } from 'src/app/models/country';
 import { AlternativesGeneratorService } from 'src/app/services/alternatives-generator.service';
 import { QuestionsGeneratorService } from 'src/app/services/questions-generator.service';
@@ -11,11 +11,14 @@ export class AlternativesViewerComponent implements OnInit {
   alternatives: Array<Country> = [];
   answer = new Country('', '', '');
   colors = ['a', 'a', 'a', 'a'];
-  rating = false;
+  disableButtons = false;
+  @Output() rate = new EventEmitter<boolean>();
+
   constructor(
     private alternativesGeneratorService: AlternativesGeneratorService,
     private questionsGeneratorService: QuestionsGeneratorService
   ) {}
+
   ngOnInit(): void {
     this.questionsGeneratorService.questionSender.subscribe((response) => {
       this.answer = response;
@@ -26,6 +29,7 @@ export class AlternativesViewerComponent implements OnInit {
         );
     });
   }
+
   findAnswerIndex() {
     let i = 0,
       found = false,
@@ -39,16 +43,19 @@ export class AlternativesViewerComponent implements OnInit {
     }
     return index;
   }
+
   rateQuestion(index: number) {
+    this.colors = ['b', 'b', 'b', 'b'];
     if (this.alternatives[index].code == this.answer.code) {
-      this.rating = true;
+      this.rate.emit(true);
       this.colors[index] = 'c';
     } else {
-      this.rating = false;
+      this.rate.emit(false);
       this.colors[index] = 'w';
       let correctIndex = this.findAnswerIndex();
       console.log(correctIndex);
       this.colors[correctIndex] = 'c';
     }
+    this.disableButtons = true;
   }
 }
