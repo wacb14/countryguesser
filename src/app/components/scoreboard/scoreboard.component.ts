348350108bd9a1b19bb7ded36b8507495e513bad
Willy Cruz <wacb14@gmail.com>
2024-04-01 13:48:53 -0500

    Formatting information display

--- src/app/components/scoreboard/scoreboard.component.ts
+++ src/app/components/scoreboard/scoreboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ThemePalette } from '@angular/material/core';
 import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
 
@@ -7,10 +7,19 @@ import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
   templateUrl: './scoreboard.component.html',
   styleUrls: ['./scoreboard.component.css'],
 })
-export class ScoreboardComponent {
+export class ScoreboardComponent implements OnInit {
+  //-- Spinner attributes
   color: ThemePalette = 'primary';
   mode: ProgressSpinnerMode = 'determinate';
-  value = 100;
+  value = 0;
   diameter = 300;
   strokeWidth = 16;
+
+  message: string = 'Congrats!';
+  rating: Array<number> = [50, 75];
+  points: string = '0';
+
+  ngOnInit(): void {
+    this.value = (this.rating[0] / this.rating[1]) * 100;
+  }
 }
