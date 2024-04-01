84179467e4fed8d157d0e2ec9b4d236c3f7f58b7
Willy Cruz <wacb14@gmail.com>
2024-03-31 22:39:00 -0500

    Scoreboard design started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ScoreboardComponent } from './components/scoreboard/scoreboard.component';
+import { CardComponent } from './components/card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CardComponent },
+  { path: 'scoreboard', component: ScoreboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
