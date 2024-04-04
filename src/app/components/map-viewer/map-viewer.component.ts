32194293a9921ab6935ab1913c7360836a0ac635
Willy Cruz <wacb14@gmail.com>
2024-04-04 16:37:17 -0500

    New component map in order to show several maps with different styles

--- src/app/components/map-viewer/map-viewer.component.ts
+++ src/app/components/map-viewer/map-viewer.component.ts
@@ -1,53 +1,11 @@
-import { AfterViewInit, Component, Input } from '@angular/core';
-import { QuestionsGeneratorService } from 'src/app/services/questions-generator.service';
-declare var $: any;
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-map-viewer',
   templateUrl: './map-viewer.component.html',
   styleUrls: ['./map-viewer.component.css'],
 })
-export class MapViewerComponent implements AfterViewInit {
-  //idMapViewer=1
+export class MapViewerComponent {
   countryCode: string = '';
   continentName: string = '';
-
-  @Input() color: string = '#f4f3f0';
-  @Input() enableZoom: boolean = true;
-  @Input() hoverOpacity: boolean = true;
-  @Input() selectedColor: string = null!;
-  @Input() colors: object = null!;
-  @Input() showTooltip: boolean = false;
-
-  constructor(private questionsGeneratorService: QuestionsGeneratorService) {}
-
-  ngAfterViewInit(): void {
-    this.questionsGeneratorService.questionSender.subscribe((response) => {
-      this.countryCode = response.code;
-      this.continentName = response.continent;
-      this.loadMap(response.continent, response.code);
-    });
-  }
-
-  //-- Changes the map showed in the card
-  loadMap(continentName: string, countryCode: string) {
-    this.emptyMapViewer();
-    var map: any = $('#mapViewer');
-    map.vectorMap({
-      map: continentName,
-      color: this.color,
-      enableZoom: this.enableZoom,
-      hoverOpacity: this.hoverOpacity,
-      normalizeFunction: 'linear',
-      selectedColor: this.selectedColor,
-      colors: this.colors,
-      showTooltip: this.showTooltip,
-    });
-    map.vectorMap('set', 'colors', countryCode, '#f63340');
-  }
-
-  //-- Clear the viewer
-  emptyMapViewer() {
-    $('#mapViewer').empty();
-  }
 }
