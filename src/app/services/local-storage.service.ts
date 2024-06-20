9f66eaf680a433a7cf5a2e6eada08196d73193a6
Willy Cruz <wacb14@gmail.com>
2024-06-20 14:13:22 -0500

    App now cannot be broken by user preferences modified from the browser

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -1,30 +1,70 @@
 import { Injectable } from '@angular/core';
+import {
+  gameModeSelectOptions,
+  continentSelectOptions,
+  questionsSelectOptions,
+} from 'src/app/models/selectsOptions';
+import { languages } from 'src/app/models/languages';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LocalStorageService {
+  gameModeSelectOptions = gameModeSelectOptions;
+  continentSelectOptions = continentSelectOptions;
+  questionsSelectOptions = questionsSelectOptions;
+  languages = languages;
+
   constructor() {}
 
   set(name: string, value: string) {
     localStorage.setItem(name, value);
   }
+  verifyValue(array: Array<any>, value: any, func: any) {
+    return array.find(func);
+  }
   get(name: string) {
     let value = localStorage.getItem(name);
-    if (value) return value;
-    else {
-      switch (name) {
-        case 'language':
-          return 'en';
-        case 'gameMode':
-          return 'maps';
-        case 'continent':
-          return 'world_en';
-        case 'numberQuestions':
-          return '5';
-      }
+    switch (name) {
+      case 'language':
+        if (
+          this.verifyValue(this.languages, value, (l: any) => {
+            return l.code == value;
+          }) &&
+          value
+        )
+          return value;
+        else return 'en';
+      case 'gameMode':
+        if (
+          this.verifyValue(this.gameModeSelectOptions, value, (o: any) => {
+            return o.value == value;
+          }) &&
+          value
+        )
+          return value;
+        else return 'maps';
+      case 'continent':
+        if (
+          this.verifyValue(this.continentSelectOptions, value, (o: any) => {
+            return o.value == value;
+          }) &&
+          value
+        )
+          return value;
+        else return 'world_en';
+      case 'numberQuestions':
+        if (
+          this.verifyValue(this.questionsSelectOptions, value, (o: any) => {
+            return o.value == value;
+          }) &&
+          value
+        )
+          return value;
+        else return '5';
+      default:
+        return '';
     }
-    return '';
   }
   remove(name: string) {
     localStorage.removeItem(name);
