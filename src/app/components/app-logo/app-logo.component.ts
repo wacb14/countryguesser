0a346196fb7ef9e83dbaf391f89593b4acc243b2
Willy Cruz <wacb14@gmail.com>
2024-07-01 22:40:08 -0500

    App logo component implemented

---
+++ src/app/components/app-logo/app-logo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app-logo',
+  templateUrl: './app-logo.component.html',
+  styleUrls: ['./app-logo.component.css']
+})
+export class AppLogoComponent {
+
+}
