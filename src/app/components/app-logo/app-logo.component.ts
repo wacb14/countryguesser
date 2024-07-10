c74d7f7cfeb249967fa264a7f0fdfd8b0fb430ee
Willy Cruz <wacb14@gmail.com>
2024-07-10 12:31:25 -0500

    App logo clickable

--- src/app/components/app-logo/app-logo.component.ts
+++ src/app/components/app-logo/app-logo.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-app-logo',
   templateUrl: './app-logo.component.html',
-  styleUrls: ['./app-logo.component.css']
+  styleUrls: ['./app-logo.component.css'],
 })
 export class AppLogoComponent {
-
+  constructor(private router: Router) {}
+  goMainMenu() {
+    this.router.navigate(['/']);
+  }
 }
